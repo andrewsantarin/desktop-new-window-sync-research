9f75986b973b0a0df71c1f2bcbc94b2319b8be5d
Cezar Andrew Villegas Santarin <andrew.santarin@gmail.com>
2019-07-18 15:18:02 +0800

    CodeSandbox can't use absolute imports?

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import 'react-app-polyfill/ie9';
 import 'antd/dist/antd.css';
 import './index.css';
 // import App from './App';
-import { Root } from 'popout-research/Root';
+import { Root } from './popout-research/Root';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
