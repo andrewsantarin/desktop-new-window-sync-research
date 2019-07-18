92a6205eeda2854dabbeb2aa5bcbab2a6f73f9c1
Cezar Andrew Villegas Santarin <andrew.santarin@gmail.com>
2019-07-18 09:21:34 +0800

    Fix menu trigger

--- src/popout-research/ThisMenuMustWork.tsx
+++ src/popout-research/ThisMenuMustWork.tsx
@@ -3,24 +3,27 @@ import { Button, Dropdown, Icon, Menu } from 'antd';
 
 export const ThisMenuMustWork = () => {
   return (
-    <Dropdown overlay={(
-      <Menu>
-        <Menu.Item key="1">
-          <Icon type="user" />
-          1st menu item
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Icon type="user" />
-          2nd menu item
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Icon type="user" />
-          3rd item
-        </Menu.Item>
-      </Menu>
-    )}>
+    <Dropdown
+      trigger={['click']}
+      overlay={(
+        <Menu>
+          <Menu.Item key="1">
+            <Icon type="user" />
+            1st menu item
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="user" />
+            2nd menu item
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Icon type="user" />
+            3rd item
+          </Menu.Item>
+        </Menu>
+      )}
+    >
       <Button>
-        Button
+        Menu
         {' '}
         <Icon type="down" />
       </Button>
